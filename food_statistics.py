ae5657e9894b9035ed7c1fdc5ccf7db985cbda54
lasilva <lucas.almeida.las@gmail.com>
2025-09-15 13:30:29 -0300

    correções da apresentação, relatório e código

--- food_statistics.py
+++ food_statistics.py
@@ -25,24 +25,33 @@ class Statistics:
             if not isinstance(value, list):
                raise TypeError("Todos os valores no dicionário do dataset devem ser listas.") 
         
+        #talvez essa condição seja desnecessária
         if dataset:
             sizes = [len(value) for value in dataset.values()]
+            #eu acho que poderia ser uma condição vendo o tamanho de um conjunto, seria mais fácil
             if not all(size == sizes[0] for size in sizes):
                 raise ValueError("Todas as colunas no dataset devem ter o mesmo tamanho.")
             
         self.dataset = dataset
 
+    #gosto de termos esse método
     def _validate_column(self, column):
         if column not in self.dataset: 
             raise KeyError(f"A coluna '{column}' não existe no dataset")
+        #eu acho que aqui vocês poderia ter feito uma validação para 
+        #ver se o conjunto não estava vazio
+        #você poderiam ter lançado uma exceção
     
     def _validade_numeric_column(self, column):
         self._validate_column(column)
         data = self.dataset[column]
 
         if data == []:
-            return
+            return #empty response pode ser problemático em algumas métricas
         
+        #essa validação é fundamental para várias das métricas
+        #vocês poderiam ter escrito-as como: 
+        #if not all(isinstance(value, (int, float)) for value in data):
         for value in data: 
             if not isinstance(value, (int, float)):
                 raise TypeError(f"A coluna '{column}' deve ter apenas valores numéricos")
@@ -67,10 +76,13 @@ class Statistics:
         self._validade_numeric_column(column)
         data = self.dataset[column]
 
+        #eu não sei se retornar zero seja o melhor nesse caso, mas entendo por causa dos testes
         if data == []:
             return 0.0
         
+        #muito bom, mas poderia colocar o retorno na mesma linha
         mean = sum(data) / len(data)
+        #return sum(data)/len(data)
         return mean
 
     def median(self, column):
@@ -89,15 +101,29 @@ class Statistics:
         float
             O valor da mediana da coluna.
         """
+        #validação muito bem feita aqui, afinal a mediana é uma 
+        #métrica de tendência central com dados numéricos. 
+
         self._validade_numeric_column(column)
         data = self.dataset[column]
-        sorted_data = sorted(data)
+        sorted_data = sorted(data) #muito bem por terem usado os 
+        #a ordenação com o sorted e não com o .sort, pois assim
+        #geramos uma cópia dos dados
 
         size = len(sorted_data)
 
-        if size < 1: return 0.0
+        if size < 1: return 0.0 #isso aqui não é muito bom
+        #não temos uma indentação correta
+        # aqui a condição poderia ter sido: 
+
+        #a verificação do tamanho poderia ter sido feita antes 
+        #da ordenação 
+        # if len(data) == 0 
 
         if size%2 == 0:
+            #aqui vocês poderiam ter removido a definição do índice 
+            #middle_index = size // 2 
+            # (sorted_data[middle_index - 1] + sorted_data[middle_index]) / 2
             return (sorted_data[size//2 - 1] + sorted_data[size//2]) / 2
         
         return sorted_data[size//2]
@@ -118,12 +144,18 @@ class Statistics:
         list
             Uma lista contendo o(s) valor(es) da moda.
         """
+        #a moda é a única das métricas de tendência central que 
+        #funciona bem com dados categóricos, ou seja, a moda 
+        #funciona para dados não numéricos. 
         self._validate_column(column)
         data = self.dataset[column]
 
+        #eu prefiro a verificação com o tamanho (len -> 0)
         if data == []:
             return []
         
+
+        # aqui era para termos um reuso da métrica de freq absoluta
         frequencia = {}
 
         mode = []
@@ -131,11 +163,14 @@ class Statistics:
         for value in data:
             if value not in frequencia: 
                 frequencia[value] = 0
-
             frequencia[value] += 1
         
+        #a metrica começaria daqui
         frequenciaMax = max(frequencia.values())
         
+        #vocês poderia reescrever essa linha da seguinte forma:
+        #mode = [i for i, j in frequencia.items() if j == frequenciaMax]
+
         for i, j in frequencia.items():
             if j == frequenciaMax: 
                 mode.append(i)
@@ -161,16 +196,26 @@ class Statistics:
         self._validade_numeric_column(column)
         data = self.dataset[column]
 
-        mean = self.mean(column)
+        #aqui você poderia ter chamado a variância
+        mean = self.mean(column) #aqui um reuso do código 
+        #muito bom
+
         sum = 0.0
 
+        #essa condição deveria estar antes de calcular a média 
+        #complicado
         if data == []: return 0.0
 
+        #aqui você poderia ter chamado a variância
         for x in data:
             sum += (x - mean) ** 2
         
         variance = sum / len(data)
+
         stdev = variance ** 0.5
+
+        #esse método todo poderia ser
+        #return self.variance(column=column) ** 0.5 
         
         return stdev
 
@@ -196,12 +241,14 @@ class Statistics:
         self._validade_numeric_column(column)
         data = self.dataset[column]
 
+        #eu acho que essa validação deveria estar um pouquinho antes
         if data == []:
             return 0.0
         
         mean_value = self.mean(column)
         #mean_value = sum(data)/len(data)
         squared_diffs = [(x - mean_value) ** 2 for x in data]
+        #poderia colocar o return direto
         variance = sum(squared_diffs) / len(data)
         return variance
 
@@ -240,6 +287,11 @@ class Statistics:
         mean_value_a = self.mean(column_a)
         mean_value_b = self.mean(column_b)
         #mean_value = sum(data)/len(data)
+
+        #aqui seria mais idiomático
+        #ListaDesvios = [(data_a - mean_value_a) * (data_b - mean_value_b) for data_a, data_b in zip(data_a, data_b)]
+
+
         for value in data_a:
             desvio_a.append(value - mean_value_a)
         for value in data_b:
@@ -250,6 +302,8 @@ class Statistics:
             desvioCorrespondente = desvio_a[i] * desvio_b[i]
             ListaDesvios.append(desvioCorrespondente)
         
+        #aqui poderia colocar direto no return 
+        #return sum(ListaDesvios) / len(data_a)
         covariance = sum(ListaDesvios) / len(data_a)
         
         return covariance
@@ -271,6 +325,8 @@ class Statistics:
         """
         self._validate_column(column)
         data = self.dataset[column]
+        #aqui você poderia já ter colocado o return 
+        #return set(data)
         itemset = set(data)
         return itemset
 
@@ -300,7 +356,6 @@ class Statistics:
         for value in data:
             if value not in absolute_frequency: 
                 absolute_frequency[value] = 0
-
             absolute_frequency[value] += 1
 
         return absolute_frequency
@@ -320,25 +375,33 @@ class Statistics:
             Um dicionário onde as chaves são os itens e os valores são
             suas proporções (frequência relativa).
         """
+        #aqui o ideal era ter chamado o método de freq absoluta no inicio 
+        #frequencia_absoluta = self.absolute_frequency(column=column)
+        #ele já faria essa validações
+
         self._validate_column(column)
         data = self.dataset[column]
 
         if data == []:
             return {}
-            
+        
+        #aqui o ideal era ter chamado o método de freq absoluta antes de tudo
         frequencia_absoluta = {}
         frequencia_relativa = {}
 
         for value in data:
             if value not in frequencia_absoluta: 
                 frequencia_absoluta[value] = 0
-
             frequencia_absoluta[value] += 1
+    
             
         totalFrequencias = sum(frequencia_absoluta.values())
 
         for value in frequencia_absoluta:
             frequencia_relativa[value] = frequencia_absoluta[value] / totalFrequencias
+
+        #aqui poderia ser simplificado
+        #return [frequencia_absoluta[value]/totalFrequencias for value in frequencia_absoluta]
 
         return frequencia_relativa 
 
@@ -363,6 +426,9 @@ class Statistics:
             Um dicionário ordenado com os itens como chaves e suas
             frequências acumuladas como valores.
         """
+
+        #aqui foi muito ruim a implementação dessa forma, pois você já tem os 
+        # métodos para calcular a freq absoluta e relativa
         frequencia_absoluta = {}
         frequencia_acumulada_absoluta = {}
         frequencia_acumulada_relativa = {}
@@ -370,21 +436,23 @@ class Statistics:
         self._validate_column(column)
         data = self.dataset[column]
 
-        data.sort()
+        data.sort() # isso é ruim, pois se você ordena a coluna e muda a ordem 
+        #dos dados no dataset original 
 
         for value in data:
             if value not in frequencia_absoluta: 
                 frequencia_absoluta[value] = 0
-
             frequencia_absoluta[value] += 1
 
         acumulador = 0
 
+        #esse cara deveria tá depois de computar a frequência
         for value in frequencia_absoluta:
             acumulador += frequencia_absoluta[value]
             frequencia_acumulada_absoluta[value] = acumulador 
             frequencia_acumulada_relativa[value] = acumulador / len(data)
         
+        #esses ifs deveria estar no inicio, chamando os métodos
         if frequency_method == "absolute":
             return frequencia_acumulada_absoluta
         elif frequency_method == "relative":
@@ -432,6 +500,7 @@ class Statistics:
             if data[i] == value2 and data[i + 1] == value1:
                 sequence_count += 1
 
+        #achei que foi positivo
         conditional_probability = sequence_count / count_value2
         return conditional_probability
     
